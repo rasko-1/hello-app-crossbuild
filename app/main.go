d3ea119b41d065018ebca7e851faf2b06af92d56
serhii.raskolei <serhii.raskolei@gmail.com>
2025-05-21 09:46:54 +0300

    Add initial Go module and main application file

---
+++ app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+	fmt.Println("This is a simple Go application.")
+	fmt.Println("It demonstrates a basic structure and functionality.")
+	fmt.Println("You can modify this code to suit your needs.")
+	fmt.Println("Feel free to add more features or improve the existing ones.")
+	fmt.Println("Happy coding!")
+}
